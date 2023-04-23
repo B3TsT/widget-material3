cd082c0c89de13fd6979848fa866c74dd4f205df
B3TsT <franciscom192@hotmail.com>
2023-04-23 08:24:37 -0700

    added menu item

---
+++ lib/src/config/theme/menu/menu_items.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart' show IconData, Icons;
+
+class MenuItem {
+  const MenuItem({
+    required this.title,
+    required this.subTitle,
+    required this.link,
+    required this.icon,
+  });
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+}
+
+const appMenuItems = <MenuItem>[
+  MenuItem(
+    title: 'Botones',
+    subTitle: 'Varios botones en Flutter',
+    link: '/buttons',
+    icon: Icons.smart_button_outlined,
+  ),
+  MenuItem(
+    title: 'Targetas',
+    subTitle: 'Un contenedor estilizado',
+    link: '/card',
+    icon: Icons.credit_card,
+  ),
+];
